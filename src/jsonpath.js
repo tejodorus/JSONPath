9d266f453f0fe505eb498affee0b173413aed31e
Theo van der Donk <t.v.d.donk@primevision.com>
2020-11-19 14:32:53 +0100

    Fixed issue 102

--- src/jsonpath.js
+++ src/jsonpath.js
@@ -790,7 +790,7 @@ JSONPath.toPathArray = function (expr) {
         return !match || !match[1] ? exp : subx[match[1]];
     });
     cache[expr] = exprList;
-    return cache[expr];
+    return cache[expr].concat();
 };
 
 export {JSONPath};
